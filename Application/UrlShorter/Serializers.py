2b4a208db45d629afd1b0cf6e4e7c070a9819f7f
sudhanshuptl <sudhanshuptl13@gmail.com>
2019-07-23 15:26:12 +0530

    updated host name resolver

--- Application/UrlShorter/Serializers.py
+++ Application/UrlShorter/Serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework.serializers import SerializerMethodField
 from .models import TinyUrl
 from .logics import base_converter_from_base10
+from django.urls import reverse
 
 
 class TinyUrlSerializer(serializers.ModelSerializer):
@@ -12,7 +13,7 @@ class TinyUrlSerializer(serializers.ModelSerializer):
         fields = ('my_url', 'url')
 
     def get_my_url(self, obj):
-        return base_converter_from_base10(obj.my_url)
+        return f'http://localhost:8000/{reverse("to_my_url", kwargs={"my_tiny_url": base_converter_from_base10(obj.my_url)})}'
 
 
 
--- Application/UrlShorter/models.py
+++ Application/UrlShorter/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class TinyUrl(models.Model):
     my_url = models.AutoField(primary_key=True)
-    url = models.CharField(max_length=200)
+    url = models.CharField(max_length=1000)
 
     class Meta:
         db_table = "tiny_url"
